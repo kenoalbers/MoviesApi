0589612ddf02d283c3581b4835f954f4eea32f4b
kenoalpe <git@kenoalbers.de>
2024-06-07 16:07:29 +0200

    feat: add shared functionality base

--- Kenoalbers.Movies.Api/Features/Movies/Endpoints/Shared/NameRequest.cs
+++ Kenoalbers.Movies.Api/Features/Movies/Endpoints/Shared/NameRequest.cs
@@ -1,7 +1,6 @@
 namespace Kenoalbers.Movies.Api.Features.Movies.Endpoints.Shared;
 
-// TODO Remove this
-public class Placeholder
+public class NameRequest
 {
     
 }
---
+++ Kenoalbers.Movies.Api/Features/Movies/Endpoints/Shared/Response.cs
@@ -0,0 +1,6 @@
+namespace Kenoalbers.Movies.Api.Features.Movies.Endpoints.Shared;
+
+public class Response
+{
+    
+}
